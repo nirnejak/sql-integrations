b7e916fa035b454e9f72c4ea29ab20a105cd344d
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-06-22 01:05:01 +0530

    Environment Type Def Added

---
+++ src/environment.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      JWT_SECRET: string
+      NODE_ENV: "development" | "production"
+      PORT?: string
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {}
