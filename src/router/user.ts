5281f82b9919d4c2b76e62353a6ea166005f9619
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-06-22 01:01:51 +0530

    User Route: Register Controller Connected

--- src/router/user.ts
+++ src/router/user.ts
@@ -5,7 +5,7 @@ import userController from "../controllers/user.controller"
 const userRouter = Router()
 
 userRouter.get("/", userController.getUsers)
-userRouter.post("/register", userController.getUsers)
+userRouter.post("/register", userController.registerUser)
 userRouter.post("/login", userController.getUsers)
 userRouter.post("/forgot-password", userController.getUsers)
 userRouter.post("/update-password", userController.getUsers)
