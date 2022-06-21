9bd391449a497f3889bed3dc42a2bf8ca8fdbbc0
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-06-22 00:07:15 +0530

    User Route Methods Updated

--- src/router/user.ts
+++ src/router/user.ts
@@ -5,9 +5,9 @@ import userController from "../controllers/user.controller"
 const userRouter = Router()
 
 userRouter.get("/", userController.getUsers)
-userRouter.get("/register", userController.getUsers)
-userRouter.get("/login", userController.getUsers)
-userRouter.get("/forgot-password", userController.getUsers)
-userRouter.get("/update-password", userController.getUsers)
+userRouter.post("/register", userController.getUsers)
+userRouter.post("/login", userController.getUsers)
+userRouter.post("/forgot-password", userController.getUsers)
+userRouter.post("/update-password", userController.getUsers)
 
 export default userRouter
