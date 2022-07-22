62debd97d8ee12e8523dab034a83ce6e3e090cd3
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-07-22 23:01:51 +0530

    Node 16.16.0

--- src/controllers/query.controller.ts
+++ src/controllers/query.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express"
+import { MongoClient } from "mongodb"
 
 /**
  * @route GET /api/query/
@@ -11,6 +12,31 @@ const getQueries = (req: Request, res: Response) => {
   res.json({ success: true })
 }
 
+/**
+ * @route GET /api/query/execute
+ * @access Public
+ * @param req Request Object
+ * @param res Response Object
+ * @description Execute a mongo query
+ */
+const executeQuery = (req: Request, res: Response) => {
+  const url = "mongodb://localhost/EmployeeDB"
+
+  MongoClient.connect(url, (err, db) => {
+    if (err) throw err
+
+    const cursor = db.collection("Employee").find(req.body.query)
+
+    cursor.each((error: any, doc: any) => {
+      if (error) throw error
+
+      console.log(doc)
+    })
+  })
+  res.json({ success: true })
+}
+
 export default {
   getQueries,
+  executeQuery,
 }
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 import { Request, Response } from "express"
+import jwt from "jsonwebtoken"
 
 const prisma = new PrismaClient()
 
@@ -48,7 +49,47 @@ const registerUser = async (
   res.json({ user })
 }
 
+type UserLoginRequest = {
+  name: string
+  email: string
+  password: string
+}
+/**
+ * @route POST /api/user/login/
+ * @access Public
+ * @param req Request Object
+ * @param res Response Object
+ * @description Login User
+ */
+const loginUser = async (
+  req: Request<{}, {}, UserLoginRequest, {}>,
+  res: Response
+) => {
+  const user = await prisma.user.findUnique({
+    where: { email: req.body.email },
+  })
+
+  // Check if user exists
+  if (!user) {
+    return res.status(401).json({ error: "User not found" })
+  }
+  // Match password
+  // return res.status(401).json({ error: "Invalid Credentials" })
+
+  jwt.sign(
+    { user },
+    process.env.JWT_SECRET,
+    { expiresIn: "1 day" },
+    (err, token) => {
+      if (err) throw err
+
+      res.json({ user: { ...user, token } })
+    }
+  )
+}
+
 export default {
   getUsers,
   registerUser,
+  loginUser,
 }
